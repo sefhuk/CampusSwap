cc3f1a26a15309d7b51f448fec8d94705eaa360e
sefhuk <ish0411@naver.com>
2023-07-24 22:17:18 +0900

    Feat: 메인 페이지 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div>Campus Swap</div>;
-}
+import React from 'react';
+import './App.css';
+import { Link } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <div className='logo' />
+      <div className='desc'>
+        <p className='text'>학우와의 소중한 거래</p>
+        <p className='text'>생필품, 교재 무엇이든!</p>
+      </div>
+      <Link className='button'>대학교 선택하기</Link>
+    </div>
+  );
+};
 
 export default App;
