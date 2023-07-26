91deb9e5bd956d134947e5282435a44423720f45
sefhuk <ish0411@naver.com>
2023-07-26 23:11:01 +0900

    Fix: Container 태그 etc prop 제거

--- src/pages/SelectCampus.jsx
+++ src/pages/SelectCampus.jsx
@@ -6,7 +6,7 @@ import { convertToHex } from '../utils/convert';
 
 const SelectCampus = () => {
   return (
-    <Container col={true} etc={''}>
+    <Container col={true}>
       <h1 className='text-3xl font-bold mt-[30%]'>대학교를 선택해주세요!</h1>
       <div className='flex flex-col w-[70%] h-[50%] mt-10'>
         <ul className='flex flex-col h-1000%] overflow-y-scroll text-xl font-bold'>
