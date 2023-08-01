e7f2931731d0ad2e2da349c05ded7a0fa8fba4a1
sefhuk <ish0411@naver.com>
2023-08-01 23:10:32 +0900

    Fix: Root 컴포넌트 export 오타 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,4 +3,6 @@ export { default as Chat } from './Chat';
 export { default as Home } from './Home';
 export { default as UserInfo } from './UserInfo';
 export { default as UploadItem } from './UploadItem';
-export { default as Main } from './Root';
+export { default as Root } from './Root';
+export { default as SignIn } from './Auth/SignIn';
+export { default as SignUp } from './Auth/SignUp';
