32a6b89d8c03f34c649d25e058e5c640b0d78ae9
sefhuk <ish0411@naver.com>
2023-08-01 12:28:19 +0900

    Fix: 로그인 여부 전역 상태 false로 수정

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const userSlice = createSlice({
   name: 'user',
   initialState: {
-    isLoggedIn: true,
+    isLoggedIn: false,
     username: null,
   },
   reducers: {
