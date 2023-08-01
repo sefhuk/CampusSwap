92eb63a1832fc4b8b837a8059a66e80cd8b818be
sefhuk <ish0411@naver.com>
2023-08-01 12:22:16 +0900

    Feat: 내정보 탭 클릭 시 로그인 정보가 없으면 로그인 페이지로 이동하도록 수정

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,14 +1,21 @@
 import NavIcon from './NavIcon';
 import { FaHome, FaPlusCircle, FaUserCog } from 'react-icons/fa';
 import { FaMessage } from 'react-icons/fa6';
+import { useSelector } from 'react-redux';
 
 const BottomNav = () => {
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+
   return (
     <nav className='flex justify-evenly items-center w-full h-[10%] '>
       <NavIcon to='' icon={() => <FaHome />} title={'홈'} />
       <NavIcon to='chat' icon={() => <FaMessage />} title={'채팅'} />
       <NavIcon to='sell' icon={() => <FaPlusCircle />} title={'판매'} />
-      <NavIcon to='my' icon={() => <FaUserCog />} title={'내정보'} />
+      <NavIcon
+        to={isLoggedIn ? 'my' : '/signin'}
+        icon={() => <FaUserCog />}
+        title={'내정보'}
+      />
     </nav>
   );
 };
