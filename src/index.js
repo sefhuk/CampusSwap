4a35d46ba775b299bd6d6dda19e58cf6d7daa171
sefhuk <ish0411@naver.com>
2023-08-02 10:27:18 +0900

    Feat: 로그인, 회원가입 페이지 라우터에 추가

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
-
-import { SelectCampus, Home, UploadItem, Chat, UserInfo } from './pages';
-import Root from './pages/Root';
+import {
+  SelectCampus,
+  Home,
+  UploadItem,
+  Chat,
+  UserInfo,
+  SignIn,
+  SignUp,
+  Root,
+} from './pages';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 const router = createBrowserRouter([
   {
@@ -22,14 +31,23 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       { path: 'chat', element: <Chat /> },
-      { path: 'my', element: <UserInfo /> },
+      {
+        path: 'my',
+        element: <UserInfo />,
+      },
     ],
   },
   {
     path: '/u/:campusId/sell',
     element: <UploadItem />,
   },
+  { path: '/signin', element: <SignIn /> },
+  { path: '/signup', element: <SignUp /> },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<RouterProvider router={router} />);
+root.render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
