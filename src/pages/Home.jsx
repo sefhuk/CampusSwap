f04f0deec8e6cc5aa2833b359d2b2916bf5763c2
sefhuk <ish0411@naver.com>
2023-08-04 16:09:20 +0900

    Feat: 구글 로그인 성공 시 Home 페이지에서 사용자 정보 전역 상태를 업데이트

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,35 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getSiginInResult } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { login } from '../redux/slice/userSlice';
+import { setParam } from '../redux/slice/urlSlice';
+
 const Home = () => {
+  const { campusId } = useParams();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setParam({ param: campusId }));
+  });
+
+  useEffect(() => {
+    getSiginInResult()
+      .then((user) => {
+        dispatch(
+          login({
+            username: user.displayName,
+            imageURL: user.photoURL,
+            email: user.email,
+          })
+        );
+      })
+      .catch(() => {
+        return;
+      });
+  }, []);
+
   return <div>Home</div>;
 };
 
